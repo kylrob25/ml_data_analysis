6d91c153cfa620d23f0713fc2223816bcd5dc09c
kylerob25 <kylrob25@icloud.com>
2023-09-27 11:54:21 +0100

    Fix selection task

--- Week Two/Part1_1/selection.py
+++ Week Two/Part1_1/selection.py
@@ -1,7 +1,40 @@
-value = -1
+# Input a positive number between 1 and 100 with error checking
+def exercise_1():
+    value = -1
 
-while value < 1 or value > 100:
-    value = int(input('Please enter a valid number between 1 and 100'))
-else:
-    even = (value % 2) == 0
-    print(even)
+    while value < 1 or value > 100:
+        value = int(input('Please enter a valid number between 1 and 100\n'))
+
+    if value % 2 == 0:
+        print(f"{value} is even")
+    else:
+        print(f"{value} is odd")
+
+
+# Input 3 numbers and an arithmetic operator
+def exercise_2():
+    total = 0
+
+    for x in range(1, 4):
+        value = int(input(f'[{x}] Enter value:\n'))
+        operator = str(input(f'[{x}] Enter operator:\n'))
+
+        match operator:
+            case '+':
+                old = total
+                total += value
+                print(f'{old} {operator} = {total}')
+            case '-':
+                old = total
+                total -= value
+                print(f'{old} {operator} = {total}')
+            case '/':
+                old = total
+                total /= value
+                print(f'{old} {operator} = {total}')
+            case '*':
+                old = total
+                total *= value
+                print(f'{old} {operator} = {total}')
+
+    print(total)
